994c17bb06f8db3a4e0146106ea8d1824d014fbd
LucF120 <lucjferrara@gmail.com>
2024-08-05 09:46:38 -0400

    Work up to page 38 of A5 done

--- Kanbas/Modules/routes.js
+++ Kanbas/Modules/routes.js
@@ -1,5 +1,16 @@
 import db from "../Database/index.js";
 export default function ModuleRoutes(app) {
+    app.put("/api/modules/:mid", (req, res) => {
+        const { mid } = req.params;
+        const module = req.body;
+        const moduleIndex = db.modules.findIndex((m) => m._id === mid);
+        if(moduleIndex === -1) {
+            res.status(404).json({ message: `Unable to delete module with ID ${mid}`});
+            return;
+        }
+        db.modules[moduleIndex] = { ...db.modules[moduleIndex], ...module};
+        res.send(204);
+    });
     app.delete("/api/modules/:mid", (req, res) => {
         const { mid } = req.params;
         const moduleIndex = db.modules.findIndex((m) => m._id === mid);
